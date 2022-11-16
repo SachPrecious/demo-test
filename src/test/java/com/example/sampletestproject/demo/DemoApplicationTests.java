5eb23dbf0e3b8db5b91745c6caa58975d151e005
Sachithra_Manamperi <61633493+SachPrecious@users.noreply.github.com>
2022-11-16 07:50:31 +0530

    Add files via upload

---
+++ src/test/java/com/example/sampletestproject/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampletestproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
